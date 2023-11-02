e095ea9c3dce8bca6a9d16521650ce3f12c8a3aa
lawrenceingithub <lawrencehei@hotmail.com>
2023-11-02 17:28:43 +0800

    what is going on

--- src/pages/cart/cart-item.jsx
+++ src/pages/cart/cart-item.jsx
@@ -2,28 +2,28 @@ import React, { useContext } from "react";
 import { ShopContext } from "../../context/shop-context";
 
 export const CartItem = (props) => {
-  const { id, productName, price, productImage, productDetail } = props.data;
-  const { cartItems, addToCart, removeFromCart, updateCartItemCount } =
+  const { productId, productname, price, picutre, detail } = props.data;
+  const { cartItems, addToCart, removeFromCart, updateCartItemCount, products } =
     useContext(ShopContext);
 
   return (
     <div className="cartItem">
-      <img src={productImage} alt={productImage}/>
+      <img src={picutre} alt={picutre}/>
       <div className="description">
         <>
         <p>
-          <b>名稱: {productName}</b><br/>
-          <b>詳情: {productDetail}</b>
+          <b>名稱: {productname}</b><br/>
+          <b>詳情: {detail}</b>
         </p>
         </>
         <p> 價值: ${price}</p>
         <div className="countHandler">
-          <button onClick={() => removeFromCart(id)}> - </button>
+          <button onClick={() => removeFromCart(productId)}> - </button>
           <input
-            value={cartItems[id]}
-            onChange={(e) => updateCartItemCount(Number(e.target.value), id)}
+            value={cartItems[productId]}
+            onChange={(e) => updateCartItemCount(Number(e.target.value), productId)}
           />
-          <button onClick={() => addToCart(id)}> + </button>
+          <button onClick={() => addToCart(productId)}> + </button>
         </div>
       </div>
     </div>
